02325b01be86cceaf20aa801a01c1866a25a931b
Crisila <crisilaaban@gmail.com>
2024-04-15 23:06:44 -0700

    added 404 page in sever, added into .gitignore, added .env file

--- Server/index.js
+++ Server/index.js
@@ -1,5 +1,25 @@
-let http = require('http');
-let server = http.createServer(function (req, res) {
-     res.write('Hello FSD!');
-     res.end();
+require('dotenv').config()
+const express = require('express');
+
+// Initialize the app
+const app = express();
+
+
+// homepage route
+app.get('/', (req, res) => {
+     res.send('Hello World!');
+})
+
+app.get('/account', (req, res) => {
+     res.send('this is the account page');
+})
+
+app.get ('*', (req, res) => {
+     res.status(404).send('<h1>Page not found</h1>');
+})
+
+
+// Listen for connections
+app.listen(process.env.PORT || 3000, () => {
+     console.log(`Server started on port ${process.env.PORT || 3000}`);
 });

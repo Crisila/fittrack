457e432d09c187284f25546ffc265771024cf2b8
Crisila <crisilaaban@gmail.com>
2024-04-23 19:15:00 -0700

    fix error version 2

--- Server/index.js
+++ Server/index.js
@@ -16,19 +16,12 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
-app.use(express.static('./build'));
+app.use(express.static('/client/build'));
 
 
-app.get('/', (req, res) => {
-     const options = {
-          root: path.join(__dirname, './build'),
-          dotfiles: 'deny',
-          headers: {
-               'x-timestamp': Date.now(),
-               'x-sent': true
-          }
-     }
-     res.sendFile('index.html', options)
+// Render client for any path
+app.get('*', (req, res) => {
+     res.sendFile(path.resolve(__dirname, '/client/build/index.html'))
 })
 
 
